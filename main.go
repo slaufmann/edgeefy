d4b2e6c01c25b4d88a4641744693ecce9c102863
Stefan Laufmann <stefan.laufmann@mailbox.tu-berlin.de>
2019-02-11 13:57:43 +0100

    adding comments to existing code

--- main.go
+++ main.go
@@ -10,9 +10,10 @@ import (
 	"os"
 )
 
+// GrayPixel is a data structure to represent the gray and alpha value of a pixel.
 type GrayPixel struct {
-	y	uint8
-	a	uint8
+	y uint8
+	a uint8
 }
 
 func main() {
@@ -32,6 +33,7 @@ func main() {
 	fmt.Println(pixels)
 }
 
+// getPixelArray reads the given file as an image and returns a two-dimensional array of GrayPixel objects.
 func getPixelArray(file io.Reader) ([][]GrayPixel, error) {
 	var pixelArr [][]GrayPixel
 
@@ -42,9 +44,9 @@ func getPixelArray(file io.Reader) ([][]GrayPixel, error) {
 	height := img.Bounds().Max.Y
 	width := img.Bounds().Max.X
 
-	for y:=0; y<height; y++ {
+	for y := 0; y < height; y++ {
 		var row []GrayPixel
-		for x:=0; x<width; x++ {
+		for x := 0; x < width; x++ {
 			pixel := img.At(x, y)
 			grayPixel := rgbaToGrayPixel(pixel)
 			row = append(row, grayPixel)
@@ -55,9 +57,10 @@ func getPixelArray(file io.Reader) ([][]GrayPixel, error) {
 	return pixelArr, nil
 }
 
+// rgbaToGrayPixel converts the given color object to a GrayPixel object.
 func rgbaToGrayPixel(pixel color.Color) GrayPixel {
 	_, _, _, a := pixel.RGBA()
 	gray := color.GrayModel.Convert(pixel).(color.Gray).Y
 
-	return GrayPixel{gray,uint8(a >> 8)}
+	return GrayPixel{gray, uint8(a >> 8)}
 }
